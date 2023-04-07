736e27a65462d08591bfdbed5a62b7f1fa235b84
WindyLibra <97620223+WindyLibra@users.noreply.github.com>
2023-04-07 10:52:24 +0300

    Create Program.cs

---
+++ c#/Tamagotchi Lite/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace klinika
+{
+    public class Windy
+    {
+        public static void Main(string[] args)
+        {
+            Petprop Astra = new Petprop("Astra");
+
+
+        }
+
+    }
+
+}
